bcbfbf0a34a6b488b5f9183c1fb75200a444d28c
D0mazo <dominykas.simanavicius@proton.me>
2025-09-08 13:40:09 +0300

    Pridėti Komentarai (naudojanti Chat GPT) Analysis.h

--- analysis/Analysis.h
+++ analysis/Analysis.h
@@ -1,14 +1,26 @@
+// Apsauga nuo daugkartinio antraštės failo įtraukimo
 #ifndef ANALYSIS_H
 #define ANALYSIS_H
 
-#include <string>
-#include <vector>
+// Įtraukiamos reikalingos bibliotekos
+#include <string>   // Darbui su eilutėmis
+#include <vector>   // Darbui su dinaminių masyvų vektoriais
 
+// Abstrakti bazinė klasė analizės operacijoms
 class Analysis {
 public:
+    // Virtualusis destruktorius, užtikrinantis tinkamą paveldinčių klasių išvalymą
     virtual ~Analysis() = default;
+
+    // Grynoji virtualioji funkcija, skirta duomenų analizei atlikti
+    // Parametras: const std::vector<double>& data - nuoroda į konstantinį dvigubo tikslumo skaičių vektorių
     virtual void run(const std::vector<double>& data) = 0;
+
+    // Grynoji virtualioji funkcija, grąžinanti analizės pavadinimą
+    // Grąžina: std::string - analizės pavadinimas
+    // const žymi, kad funkcija nekeičia objekto būsenos
     virtual std::string name() const = 0;
 };
 
+// Antraštės apsaugos pabaiga
 #endif
